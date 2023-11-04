a282358eed7fc421d07b5f7533809565b869159b
Gabriel Jesus <gabriel.developer2@gmail.com>
2023-11-04 14:20:02 -0300

    Animated Shake component CARD.

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -8,11 +8,12 @@ import { styles } from './styles';
 import { QUIZ } from '../../data/quiz';
 import { historyAdd } from '../../storage/quizHistoryStorage';
 
+import Animated, { Easing, interpolate, useAnimatedStyle, useSharedValue, withSequence, withTiming } from 'react-native-reanimated';
+import { ConfirmButton } from '../../components/ConfirmButton';
 import { Loading } from '../../components/Loading';
+import { OutlineButton } from '../../components/OutlineButton';
 import { Question } from '../../components/Question';
 import { QuizHeader } from '../../components/QuizHeader';
-import { ConfirmButton } from '../../components/ConfirmButton';
-import { OutlineButton } from '../../components/OutlineButton';
 
 interface Params {
   id: string;
@@ -31,7 +32,16 @@ export function Quiz() {
 
   const route = useRoute();
   const { id } = route.params as Params;
-
+  const shake = useSharedValue(0)
+  const shakeStyleAnimated = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: interpolate(
+        shake.value,
+        [0, 0.5, 1, 1.5, 2, 2.5, 3],
+        [0, -15, 0, 15, 0, -15, 0],
+      ) }]
+    }
+  })
   function handleSkipConfirm() {
     Alert.alert('Pular', 'Deseja realmente pular a questão?', [
       { text: 'Sim', onPress: () => handleNextQuestion() },
@@ -69,6 +79,8 @@ export function Quiz() {
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
       setPoints(prevState => prevState + 1);
+    } else {
+      shakeAnimation()
     }
 
     setAlternativeSelected(null);
@@ -89,6 +101,15 @@ export function Quiz() {
 
     return true;
   }
+
+  function shakeAnimation() {
+    shake.value = withSequence(
+      withTiming(3, { duration: 400, easing: Easing.bounce }), 
+      withTiming(0)
+    )
+  }
+
+
 
   useEffect(() => {
     const quizSelected = QUIZ.filter(item => item.id === id)[0];
@@ -118,12 +139,16 @@ export function Quiz() {
           totalOfQuestions={quiz.questions.length}
         />
 
-        <Question
-          key={quiz.questions[currentQuestion].title}
-          question={quiz.questions[currentQuestion]}
-          alternativeSelected={alternativeSelected}
-          setAlternativeSelected={setAlternativeSelected}
-        />
+        <Animated.View
+          style={shakeStyleAnimated}
+        >
+          <Question
+            key={quiz.questions[currentQuestion].title}
+            question={quiz.questions[currentQuestion]}
+            alternativeSelected={alternativeSelected}
+            setAlternativeSelected={setAlternativeSelected}
+          />
+        </Animated.View>
 
         <View style={styles.footer}>
           <OutlineButton title="Parar" onPress={handleStop} />
